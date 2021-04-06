3fda64b396fd7c1168981bf81a1930c28ce4dadf
Bruno Lima <blima@br.ibm.com>
2021-04-06 19:29:05 -0300

    it creates a method to load a file data to a pandas dataframe

--- notebooks/data-ingestion.py
+++ notebooks/data-ingestion.py
@@ -51,6 +51,18 @@ def check_dir(data_dir):
         raise Exception("specified data dir does not contain any files")
 
 
+def read_data_file(data_file_path):
+    """
+    Method used to read a data file located on the given path
+
+    :param data_file_path: Data file full path
+    :type data_file_path: str
+
+    :return: pd.DataFrame -- Data load to a dataframe object
+    """
+    return pd.read_json(data_file_path)
+
+
 def fetch_data(data_dir):
     """
     Method used to read data from json files and add it to a dataframe
@@ -62,9 +74,10 @@ def fetch_data(data_dir):
     """
     check_dir(data_dir)
 
+    data = list()
     for data_file in os.listdir(data_dir):
-        data_file = normalize_path([data_dir, data_file])
-        print(data_file)
+        data_file_path = normalize_path([data_dir, data_file])
+        data.append(read_data_file(data_file_path))
 
 
 fetch_data(DATA_TRAIN_DIR)
