c25434db172ab6b60d939530ad4b82a3e5e85cdd
Bruno Lima <blima@br.ibm.com>
2021-04-19 20:27:46 -0300

    it aggregate features by day and country in feature engineering phase

--- notebooks/feature_engineering.py
+++ notebooks/feature_engineering.py
@@ -16,7 +16,23 @@ DATA_INPUT_DIR = normalize_path([BUILD_DIR, "data-ingested.csv"])
 
 # %%
 
-data = pd.read_csv(DATA_INPUT_DIR, parse_dates=["datetime"])
-data.info()
+invoices = pd.read_csv(DATA_INPUT_DIR, parse_dates=["datetime"])
+invoices.info()
+
+# %%
+
+invoices_by_day = (
+    invoices.groupby([pd.Grouper(key="datetime", freq="D"), "country"])
+    .agg(
+        purchases=("invoice", "count"),
+        num_invoices=("invoice", pd.Series.nunique),
+        num_streams=("stream_id", "count"),
+        num_views=("times_viewed", "count"),
+        revenue=("price", "sum"),
+    )
+    .reset_index()
+)
+
+invoices_by_day.head()
 
 # %%
