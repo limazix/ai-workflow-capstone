0c621143061f4740368e9a67a2ff84ad8401dd18
Bruno Lima <blima@br.ibm.com>
2021-04-19 18:33:51 -0300

    it creates a tool to handle path operations

--- notebooks/data-ingestion.py
+++ notebooks/data-ingestion.py
@@ -5,6 +5,8 @@ import warnings
 
 import pandas as pd
 
+from tools.path_tools import check_dir, normalize_path
+
 warnings.filterwarnings("ignore")
 
 # %%
@@ -14,33 +16,6 @@ DATA_TRAIN_DIR = os.path.normpath(os.path.join(DATA_ROOT_DIR, "cs-train"))
 DATA_OUTPUT_DIR = os.path.normpath(os.path.join("..", "build"))
 
 # %%
-
-
-def normalize_path(parts: list):
-    """
-    Method used to join and format a given list of a path's parts
-
-    :param parts: List of path's parts
-    :type parts: list
-
-    :return: str -- The normalized path
-    """
-    return os.path.normpath(os.path.join(*parts))
-
-
-def check_dir(data_dir):
-    """
-    Method used to validate the given data directory path
-
-    :param data_dir: Absolute path for the data directory
-    :type data_dir: str
-
-    :raise: Exception
-    """
-    if not os.path.isdir(data_dir):
-        raise Exception("specified data dir does not exist")
-    if not len(os.listdir(data_dir)) > 0:
-        raise Exception("specified data dir does not contain any files")
 
 
 def parse_column_names(df):
